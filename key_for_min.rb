0458351d869154b59214e1ee0dead00252a13bf8
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-17 14:48:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  
+ comparison = 10000
+ 
+  if name_hash == {}
+    return nil
+  end
+   
+   name_hash.collect do |key, value|
+     if value < comparison
+       comparison = key
+     end
+   comparison
 end
